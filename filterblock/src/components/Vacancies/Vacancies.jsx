365a79e9eaf6dabc00a8ad810afcc0dd668ec188
AntonMiranovich <antonmir992@gmail.com>
2024-02-25 23:29:14 +0300

    add paginations

--- filterblock/src/components/Vacancies/Vacancies.jsx
+++ filterblock/src/components/Vacancies/Vacancies.jsx
@@ -3,28 +3,41 @@ import { IconMapPin } from "@tabler/icons-react";
 import { Pagination } from "@mantine/core";
 import arrObj from "../../Storage/Storage.json";
 import { Link } from "react-router-dom";
+import {useState } from "react";
 
 function Vacancies() {
+  const [vacanciesOnThePage] = useState(4);
+  const [paginalPageNumber, setPaginalPageNumber] = useState(1);
+
+  const lastIndex = vacanciesOnThePage * paginalPageNumber;
+  const firstIndex = lastIndex - vacanciesOnThePage;
+  const displayedArray = arrObj.slice(firstIndex, lastIndex);
+
   return (
     <div className={style.vacancies}>
       <div className={style.list}>
-        {arrObj.map((el) => (
+        {displayedArray.map((el) => (
           <Link key={el.id} to={`/${el.id}`}>
             <div className={style.wrapper}>
               <h1>{el.title}</h1>
-              <div className={style.text}>
+              <div className={style.text} >
                 <p>{el.salary}</p>
                 <p className={style.work}>{el.time}</p>
               </div>
               <div className={style.flexLocation}>
-                <IconMapPin />
+                <IconMapPin/>
                 <p>{el.location}</p>
               </div>
             </div>
           </Link>
         ))}
       </div>
-      <Pagination total={10} />;
+      <Pagination
+        total={Math.ceil(arrObj.length / vacanciesOnThePage)}
+        onChange={setPaginalPageNumber}
+        mt="sm"
+      />
+      ;
     </div>
   );
 }
